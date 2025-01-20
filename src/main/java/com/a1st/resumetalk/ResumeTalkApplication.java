8274bf45446e07b0a2fed7a573ae4fdefc640169
Abderrahman Youabd <youabd50@gmail.com>
2025-01-20 11:34:33 +0100

    initial commit

---
+++ src/main/java/com/a1st/resumetalk/ResumeTalkApplication.java
@@ -0,0 +1,13 @@
+package com.a1st.resumetalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/a1st/resumetalk/ResumeTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a1st.resumetalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
